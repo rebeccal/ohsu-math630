1d923eb2934ed67db2e48b58d315730972509b3c
Rebecca Lunsford <rebecca@luns4d.com>
2018-09-28 08:09:59 -0700

    add video

--- RL_RSamples/1-2.R
+++ RL_RSamples/1-2.R
@@ -4,19 +4,30 @@ bakers_dozen <- read_csv("http://bit.ly/conj620-bakers-dozen")
 our_data <- bakers_dozen %>%
 filter(dataset==1)
 
+# One way
 our_data %>%
-summarize_all(funs(mean, sd))
+  summarise(x_mean=mean(x), x_sd=sd(x), y_mean=mean(y), y_sd=sd(y))
+
+# Another way
+summarise(our_data, x_mean=mean(x), x_sd=sd(x), y_mean=mean(y), y_sd=sd(y))
+
+# Yet another
+summarise(our_data, mean(x), sd(x), mean(y), sd(y))
+
+#And...
+our_data %>%
+  summarize_all(funs(mean, sd))
+
 
 our_data %>% 
 ggplot() +
   geom_point(aes(x,y))
 
 
-# Mine
-
+# All
 bakers_dozen %>%
-group_by(dataset) %>%
-summarize_all(funs(mean, sd))
+  group_by(dataset) %>%
+  summarize_all(funs(mean, sd))
 
 
 bakers_dozen %>% 
