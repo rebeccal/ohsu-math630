02703fb9b69f3cbafca5a6062ac76e7d6ef0f07e
Rebecca Lunsford <rebecca@luns4d.com>
2018-10-09 09:22:12 -0700

    More code-along

--- RL_RSamples/LM-extras.R
+++ RL_RSamples/LM-extras.R
@@ -19,8 +19,28 @@ sqrt(diag(solve(crossprod(design_matrix))) * rse2)
 lm_swiss_aug <- augment(lm_swiss)
 lm_swiss_pts <- moderndive::get_regression_points(lm_swiss)
 
-lm_swiss_aug # Using augment for direct comparison of hat
 
+# cov, SSs, r, R2
+cov_swiss <- with(swiss,cov(Fertility, Education))
+cor2_swiss <- cov_swiss/sd(swiss$Fertility)*sd(swiss$Education) # nope
+cor2_swiss
+cor_swiss <- with(swiss,cor(Fertility, Education))
+cor_swiss
+
+R2_swiss <-  cor_swiss^2
+
+
+swiss_ss <- lm_swiss_aug %>% 
+  summarise(total_ss = sum((Fertility - mean(Fertility))^2),
+            resid_ss = sum((Fertility - .fitted)^2), 
+            model_ss = sum((.fitted - mean(Fertility))^2),
+            total = resid_ss + model_ss,
+            rsq = model_ss / total_ss,
+            var_est = total_ss / (n()-1),
+            swiss_var = var(Fertility))
+
+
+lm_swiss_aug # Using augment for direct comparison of hat
 #Hat 
 X <- design_matrix
 #hat_matrix <- X%*%ginv(t(X)%*%X)%*%t(X) #Same
