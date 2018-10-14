7f47a4b932830d02ea4ea19149f1995085664774
Rebecca Lunsford <rebecca@luns4d.com>
2018-10-14 12:59:58 -0700

    New slide deck cm025. Based on 2017 slide deck 9, cm025 slides and in-class lab, and cm026 slides.

---
+++ RL_RSamples/3DPlot.R
@@ -0,0 +1,43 @@
+
+advertising <- read_csv("http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv")
+
+#Fixed - but cool!!
+advertising_fit1 <- lm(sales~TV+radio, data = advertising)
+sp <- scatterplot3d::scatterplot3d(advertising$TV, 
+                                   advertising$radio, 
+                                   advertising$sales, 
+                                   angle = 45)
+sp$plane3d(advertising_fit1, lty.box = "solid")#,
+# polygon_args = list(col = rgb(.1, .2, .7, .5)) # Fill color
+orig <- sp$xyz.convert(advertising$TV, 
+                       advertising$radio, 
+                       advertising$sales)
+plane <- sp$xyz.convert(advertising$TV, 
+                        advertising$radio,  fitted(advertising_fit1))
+i.negpos <- 1 + (resid(advertising_fit1) > 0)
+segments(orig$x, orig$y, plane$x, plane$y,
+         col = c("blue", "red")[i.negpos], 
+         lty = 1) # (2:1)[i.negpos]
+sp <- FactoClass::addgrids3d(advertising$TV, 
+                             advertising$radio, 
+                             advertising$sales,
+                             angle = 45,
+                             grid = c("xy", "xz", "yz"))
+
+
+##Interactive
+rgl::plot3d(advertising$TV, 
+            advertising$radio, 
+            advertising$sales, type = "p", 
+            xlab = "TV", 
+            ylab = "radio", 
+            zlab = "Sales", site = 5, lwd = 15)
+rgl::planes3d(advertising_fit1$coefficients["TV"], 
+              advertising_fit1$coefficients["radio"], -1, 
+              advertising_fit1$coefficients["(Intercept)"], alpha = 0.3, front = "line")
+rgl::segments3d(rep(advertising$TV, each = 2), 
+                rep(advertising$radio, each = 2),
+                matrix(t(cbind(advertising$sales, predict(advertising_fit1))), nc = 1),
+                col = c("blue", "red")[i.negpos], 
+                lty = 1) # (2:1)[i.negpos]
+#rgl::rgl.postscript("./pics/plot-advertising-rgl.pdf","pdf") # does not really work...
--- RL_RSamples/LM-extras.R
+++ RL_RSamples/LM-extras.R
@@ -1,16 +1,38 @@
 
 library(tidyverse)
+library(MASS) #ginv() matrix inversion
+library(broom)
+library(moderndive)
 
-ggplot(swiss,aes(Agriculture, Fertility)) +
-geom_smooth(method="lm") +
-geom_point()
 
-lm_swiss <-lm(Fertility ~Education,swiss)
-summary(lm_swiss)
+GGally::ggpairs(swiss)
+
+swiss %>%
+  cor()  %>% 
+  round(3)
+  
+
+swiss %>%
+  cor(.,use = "pairwise.complete.obs")
+
+swiss %>%
+  corrr::correlate()
+
+
+ggplot(swiss,aes(x=Examination, y=Agriculture)) +
+  geom_smooth(method="lm") +
+  geom_point() +
+  geom_text(aes(label=rownames(swiss)))
+
+
+lm_swiss <-lm(Agriculture ~Examination,swiss)
+tidy(lm_swiss)
+glance(lm_swiss)
+
 
 # Residual standard error and standard error of the coefficients
 design_matrix <- model.matrix(lm_swiss)
-rse2 <- sum((swiss$Fertility - fitted(lm_swiss))^2) / (nrow(design_matrix) - ncol(design_matrix))
+rse2 <- sum((swiss$Agriculture - fitted(lm_swiss))^2) / (nrow(design_matrix) - ncol(design_matrix))
 sqrt(rse2)
 sqrt(diag(solve(crossprod(design_matrix))) * rse2)
 
@@ -21,33 +43,93 @@ lm_swiss_pts <- moderndive::get_regression_points(lm_swiss)
 
 
 # cov, SSs, r, R2
-cov_swiss <- with(swiss,cov(Fertility, Education))
-cor2_swiss <- cov_swiss/sd(swiss$Fertility)*sd(swiss$Education) # nope
-cor2_swiss
-cor_swiss <- with(swiss,cor(Fertility, Education))
+dev_swiss <- swiss %>% 
+  mutate(dev_Y = (Agriculture - mean(Agriculture)), 
+         dev_X = (Examination - mean(Examination)),
+         var_YX = dev_Y * dev_X)
+
+cov_swiss_byhand <- sum(dev_swiss$var_YX)/(nrow(dev_swiss)-1)
+cov_swiss_byhand
+cov_swiss <- with(swiss,cov(Agriculture, Examination)) 
+cov_swiss #Woot, they match!
+
+cor_swiss <- with(swiss,cor(Agriculture, Examination))
 cor_swiss
+sd_Y <- sd(swiss$Agriculture)
+sd_X <- sd(swiss$Examination)
+cor2_swiss <- cov_swiss/(sd_Y * sd_X) 
+cor2_swiss # They match!!
 
 R2_swiss <-  cor_swiss^2
-
+R2_swiss
+glance(lm_swiss)
 
 swiss_ss <- lm_swiss_aug %>% 
-  summarise(total_ss = sum((Fertility - mean(Fertility))^2),
-            resid_ss = sum((Fertility - .fitted)^2), 
-            model_ss = sum((.fitted - mean(Fertility))^2),
+  summarise(total_ss = sum((Agriculture - mean(Agriculture))^2),
+            resid_ss = sum((Agriculture - .fitted)^2), 
+            model_ss = sum((.fitted - mean(Agriculture))^2),
             total = resid_ss + model_ss,
             rsq = model_ss / total_ss,
             var_est = total_ss / (n()-1),
-            swiss_var = var(Fertility))
+            swiss_var = var(Agriculture))
+
+swiss_ss$rsq #and they match
 
 
 lm_swiss_aug # Using augment for direct comparison of hat
-#Hat 
+
+#Hat
 X <- design_matrix
-#hat_matrix <- X%*%ginv(t(X)%*%X)%*%t(X) #Same
+ols_estimator <- ginv(t(X)%*%X)%*%t(X) #dreivable, but yuck!
+hat_matrix <- X%*%ols_estimator 
 #hat_matrix <- X%*%ginv(crossprod(X))%*%t(X) #Same
-hat_matrix <- X%*%solve(crossprod(X))%*%t(X)
-aug_hat <- diag(hat_matrix)
+#hat_matrix <- X%*%solve(crossprod(X))%*%t(X) #Same
+hat <- diag(hat_matrix)
+n_parameters <- sum(hat) #Including the intercept
+
+SS_ex <- sum((swiss$Examination-mean(swiss$Examination))^2)
+hat_ish <- lm_swiss_aug %>% 
+  mutate(.hatish = (1/n()+(Examination-mean(Examination))^2/SS_ex))
+hat_ish #matches!
 
 #Fitted
+betas <- ols_estimator%*%swiss$Fertility 
 lm_swiss_fitted <-hat_matrix %*% lm_swiss_aug$Fertility
+
+
+#More Outliers - discrepancy via studentized residuals
+
+SE <- glance(lm_swiss)$sigma #SE of the residuals
+hat_ish <- hat_ish %>%
+  mutate(isr = .resid/(SE*sqrt(1-.hat))) # Internalized student residual - matches augment's .std.resid
+
+hat_ish <- hat_ish %>%
+  mutate(isr2 = .resid/sd(.resid)) # Internalized student residual ~ matches augment's .std.resid
+
+hat_ish <- hat_ish %>%
+  mutate(esr = rstudent(lm_swiss)) # Externalized student residual. NOT augment's .std.resid
+
+
+car::outlierTest(lm_swiss) #outlier test with bonferroni correction
+
+
+
+#More hat calcs
+#1 minus the ratio of residuals with and without the point
+
+fit = lm(mpg ~ wt, mtcars) # OLS including all points
+X2 = model.matrix(fit)     # X model matrix
+hat2_matrix = X2%*%(solve(t(X2)%*%X2)%*%t(X2)) # Hat matrix
+diag(hat2_matrix)[1] # First diagonal point in Hat matrix
+
+fitwithout1 = lm(mpg ~ wt, mtcars[-1,]) # OLS excluding first data point.
+new = data.frame(wt=mtcars[1,'wt']) # Predicting y hat in this OLS w/o first point.
+y_hat_without = predict(fitwithout1, newdata=new) # ... here it is.
+
+residuals(fit)[1] # The residual when OLS includes data point.
+lev = 1 - (residuals(fit)[1]/(mtcars[1,'mpg'] -  y_hat_without)) # Leverage
+all.equal(diag(hat2_matrix)[1],lev) #TRUE
+
+augment(fit)
+
 
